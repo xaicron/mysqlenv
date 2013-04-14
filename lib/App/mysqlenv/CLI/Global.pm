f9e3f25e8336d88c7c622ee312a0736707e1d47a
xaicron <xaicron@gmail.com>
2013-04-15 04:30:38 +0900

    cleanup

--- lib/App/mysqlenv/CLI/Global.pm
+++ lib/App/mysqlenv/CLI/Global.pm
@@ -7,14 +7,14 @@ use App::mysqlenv::Logger;
 
 sub run {
     my ($class, @argv) = @_;
-    my $version_file = catdir mysqlenv_home, 'version';
+    my $version_file = find_global_mysqlenv_version_file;
 
     my $global_version = '';
     unless (-f $version_file) {
         $global_version = 'no set';
     }
     else {
-        ($global_version) = read_file $version_file;
+        $global_version = slurp_version($version_file);
     }
 
     my $new_version = shift @argv;

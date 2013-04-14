968760bc0c8313aa8d9430f21aaf71870c395c4d
xaicron <xaicron@gmail.com>
2013-04-15 04:03:44 +0900

    implementes Install

--- lib/App/mysqlenv/CLI/Install.pm
+++ lib/App/mysqlenv/CLI/Install.pm
@@ -2,10 +2,48 @@ package App::mysqlenv::CLI::Install;
 
 use strict;
 use warnings;
+use App::mysqlenv::Util;
+use App::mysqlenv::Logger;
+use App::mysqlenv::Getopt;
 
 sub run {
-    my ($class, @commands) = @_;
-    warn join "\n", @commands;
+    my ($class, @args) = @_;
+    my $install_home = install_home;
+
+    App::mysqlenv::Getopt->parse_options(
+        \@args => (
+            'v|verbose!' => \my $verbose,
+            '--as=s'     => \my $as,
+        ),
+    );
+
+    my $version = shift @args || show_usage('Install');
+
+    my $install_path = catdir $install_home, $as || $version;
+    if (-d $install_path) {
+        errorf '%s is exists', $install_path;
+    }
+
+    command 'mysql-build',
+        ($verbose ? '-v' : ()),
+        $version, $install_path,
+        (@args ? ('--', @args) : ()),
+    ;
+
+    chdir $install_path or die "$!: $install_path";
+    command './scripts/mysql_install_db';
+
+    infof 'Successfully install!';
+
+    print << "NOTE";
+
+You can try
+
+\$ mysqlenv global $version
+
+NOTE
+
+    return 1;
 }
 
 1;
--- lib/App/mysqlenv/Util.pm
+++ lib/App/mysqlenv/Util.pm
@@ -4,28 +4,54 @@ use strict;
 use warnings;
 use parent 'Exporter';
 
-use Cwd;
-use File::Path;
-use File::Spec::Functions;
-use File::Slurp;
+use Cwd qw(getcwd);
+use File::Basename qw(basename);
+use File::Path qw(mkpath rmtree);
+use File::Spec::Functions qw(catdir catfile rel2abs);
+use File::Slurp qw(write_file read_file);
 
-use App::mysqlenv;
+use App::mysqlenv ();
 use App::mysqlenv::Logger;
 
 our @EXPORT = qw{
-    command mkpath rmtree catdir getcwd
-    write_file read_file
+    mkpath
+    rmtree
+    catdir
+    catfile
+    rel2abs
+    basename
+    getcwd
+    write_file
+    read_file
+    command
     mysqlenv_home
+    install_home
+    current_version
+    show_usage
 };
 
 sub mysqlenv_home {
     App::mysqlenv->home;
 }
 
+sub install_home {
+    catdir mysqlenv_home(), 'mysqls';
+}
+
+sub current_version {
+    $ENV{MYSQLENV_VERSION} || '-';
+}
+
 sub command {
     my @commands = @_;
-    infof 'run %s', join ' ', @_;
-    !system(@_) or die "$!";
+    infof 'run: %s', join ' ', @_;
+    !system(@_) or die "$?";
+}
+
+sub show_usage {
+    require App::mysqlenv::CLI::Help;
+    App::mysqlenv::CLI::Help->run(@_);
+    exit 1;
 }
 
 1;

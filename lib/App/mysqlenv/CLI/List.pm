d375ad274f359f4642b23692b85f1a439fa853a8
xaicron <xaicron@gmail.com>
2013-04-15 04:04:21 +0900

    implementes list

--- lib/App/mysqlenv/CLI/List.pm
+++ lib/App/mysqlenv/CLI/List.pm
@@ -2,10 +2,26 @@ package App::mysqlenv::CLI::List;
 
 use strict;
 use warnings;
+use App::mysqlenv::Util;
+use App::mysqlenv::Logger;
 
 sub run {
-    my ($class, @commands) = @_;
-    warn join "\n", @commands;
+    my ($class, @argv) = @_;
+    my $install_home = install_home;
+
+    my $current_version = current_version;
+    chdir $install_home or die "$!: $install_home";
+    opendir my $dh, '.' or die "$!: $install_home";
+    for my $version (grep !/^\./ && -d, readdir $dh) {
+        if ($version eq $current_version) {
+            print "* $version\n";
+        }
+        else {
+            print "  $version\n";
+        }
+    }
+
+    return 1;
 }
 
 1;

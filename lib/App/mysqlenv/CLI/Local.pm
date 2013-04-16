3b1ba735d92918eddc704b126259f981cc865c4b
xaicron <xaicron@gmail.com>
2013-04-17 05:57:02 +0900

    implemeted local

--- lib/App/mysqlenv/CLI/Local.pm
+++ lib/App/mysqlenv/CLI/Local.pm
@@ -7,8 +7,24 @@ use App::mysqlenv::Logger;
 
 sub run {
     my ($class, @argv) = @_;
-    # TODO
-    errorf 'Not implemeted';
+
+    my $version = shift @argv;
+    if ($version) {
+        unless (-d catfile install_home, $version) {
+            errorf '[mysqlenv] %s is not installed', $version;
+        }
+        write_file '.mysql-version', $version;
+    }
+    else {
+        if (my $file = find_lcoal_mysqlenv_version_file) {
+            print slurp_version($file), "\n";
+        }
+        else {
+            errorf '[mysqlenv] no local version configured for this directory';
+        }
+    }
+
+    return 1;
 }
 
 1;
@@ -21,5 +37,5 @@ App::mysqlenv::CLI::SelfInstall - Setes local mysql version under the directory
 
 =head1 SYNOPSIS
 
-    mysqlenv local <version>
+    mysqlenv local [version]
 
